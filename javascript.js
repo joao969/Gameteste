b8e0036ad20f4e6ed666ca5590e5d3e769b4d8f0
joao969 <joao_marcos3@hotmail.com>
2024-02-28 20:11:03 -0300

    att6

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const zeldinha = document.querySelector('.zeldinha');
 const chefao = document.querySelector('.chefao');
-const fantasma = document.querySelector('.fantasma');
+const esqueleto = document.querySelector('.esqueleto');
 const gameover1 = document.querySelector('.gameover1');
 const gameover2 = document.querySelector('.gameover2');
 const gameover3 = document.querySelector('.gameover3');
@@ -14,14 +14,46 @@ const jump = () => {
         zeldinha.classList.remove('jump');
     }, 500);
 }
+
+
+
 const loop = setInterval(() => {
+
+    const esqueletoPosition = esqueleto.offsetLeft;
+    const zeldinhaPosition = +window.getComputedStyle(zeldinha).bottom.replace('px', '');
+
+    console.log(zeldinhaPosition);
+
+    if (esqueletoPosition <= 100 && esqueletoPosition > 0 && zeldinhaPosition < 100) {
+
+        esqueleto.style.animation = 'none';
+        esqueleto.style.left = `${esqueletoPosition}px`;
+
+        zeldinha.style.animation = 'none';
+        zeldinha.style.bottom = `${zeldinhaPosition}px`;
+
+        zeldinha.src = '../Gameteste/imagens/fantasma.gif';
+        zeldinha.style.top = '200px'
+        fim.textContent = 'GAME OVER';
+        reiniciar.textContent = 'REINICIAR';
+        
+
+        clearInterval(loop);
+    }
+
+}, 100);
+
+
+
+
+const loop2 = setInterval(() => {
 
     const chefaoPosition = chefao.offsetLeft;
     const zeldinhaPosition = +window.getComputedStyle(zeldinha).bottom.replace('px', '');
 
     console.log(zeldinhaPosition);
 
-    if (chefaoPosition <= 20 && chefaoPosition > 0 && zeldinhaPosition < 100) {
+    if (chefaoPosition <= 120 && chefaoPosition > 0 && zeldinhaPosition < 100) {
 
         chefao.style.animation = 'none';
         chefao.style.left = `${chefaoPosition}px`;
@@ -39,6 +71,11 @@ const loop = setInterval(() => {
     }
 
 }, 100);
+
+
+
+
+
 document.addEventListener('keydown', jump);
 
 
