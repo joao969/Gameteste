147e7d4dc3af77c2ae6821b88500a0652a6b5307
joao969 <joao_marcos3@hotmail.com>
2024-02-29 12:03:34 -0300

    att7

--- javascript.js
+++ javascript.js
@@ -1,11 +1,10 @@
 const zeldinha = document.querySelector('.zeldinha');
 const chefao = document.querySelector('.chefao');
 const esqueleto = document.querySelector('.esqueleto');
-const gameover1 = document.querySelector('.gameover1');
-const gameover2 = document.querySelector('.gameover2');
-const gameover3 = document.querySelector('.gameover3');
+
 const fim = document.querySelector('.fim');
 const reiniciar = document.querySelector('.reiniciar');
+
 
 const jump = () => {
     zeldinha.classList.add('jump');
@@ -15,8 +14,6 @@ const jump = () => {
     }, 500);
 }
 
-
-
 const loop = setInterval(() => {
 
     const esqueletoPosition = esqueleto.offsetLeft;
@@ -24,7 +21,7 @@ const loop = setInterval(() => {
 
     console.log(zeldinhaPosition);
 
-    if (esqueletoPosition <= 100 && esqueletoPosition > 0 && zeldinhaPosition < 100) {
+    if (esqueletoPosition <= 140 && esqueletoPosition > 0 && zeldinhaPosition < 100) {
 
         esqueleto.style.animation = 'none';
         esqueleto.style.left = `${esqueletoPosition}px`;
@@ -32,37 +29,11 @@ const loop = setInterval(() => {
         zeldinha.style.animation = 'none';
         zeldinha.style.bottom = `${zeldinhaPosition}px`;
 
-        zeldinha.src = '../Gameteste/imagens/fantasma.gif';
-        zeldinha.style.top = '200px'
-        fim.textContent = 'GAME OVER';
-        reiniciar.textContent = 'REINICIAR';
-        
-
-        clearInterval(loop);
-    }
-
-}, 100);
-
-
-
-
-const loop2 = setInterval(() => {
-
-    const chefaoPosition = chefao.offsetLeft;
-    const zeldinhaPosition = +window.getComputedStyle(zeldinha).bottom.replace('px', '');
-
-    console.log(zeldinhaPosition);
-
-    if (chefaoPosition <= 120 && chefaoPosition > 0 && zeldinhaPosition < 100) {
-
-        chefao.style.animation = 'none';
-        chefao.style.left = `${chefaoPosition}px`;
-
-        zeldinha.style.animation = 'none';
-        zeldinha.style.bottom = `${zeldinhaPosition}px`;
-
-        zeldinha.src = '../Gameteste/imagens/fantasma.gif';
+        esqueleto.src = '../Gameteste/imagens/fogo2.gif'
+        esqueleto.style.left = '10px'
+        zeldinha.src = '../Gameteste/imagens/perso2.gif';
         zeldinha.style.top = '450px'
+        zeldinha.style.width = '150px'
         fim.textContent = 'GAME OVER';
         reiniciar.textContent = 'REINICIAR';
         
@@ -71,8 +42,6 @@ const loop2 = setInterval(() => {
     }
 
 }, 100);
-
-
 
 
 
